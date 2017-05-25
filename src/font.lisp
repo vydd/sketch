69d0de40fec87aa36217e609440b689df0fd0e60
vydd <vidovicd@gmail.com>
2017-05-25 17:33:46 +0200

    Multiline text, alignment, line-height, fixes #9
    Finishes work on multiline text, now works with custom width & height.
    Also introduces FONT-LINE-HEIGHT and FONT-ALIGN, because why not.

--- src/font.lisp
+++ src/font.lisp
@@ -11,16 +11,20 @@
 (defclass font (resource)
   ((face :accessor font-face :initarg :face)
    (color :accessor font-color :initarg :color)
-   (size :accessor font-size :initarg :size)))
+   (size :accessor font-size :initarg :size :initform 16)
+   (line-height :accessor font-line-height :initarg :line-height :initform 1.41)
+   (align :accessor font-align :initarg :align :initform :left)))
 
-(defun make-font (&key face color size)
+(defun make-font (&key face color size line-height align)
   (let* ((*env* (or *env* (make-env))))
     (make-instance 'font
                    :face (or face
                              (font-face (or (env-font *env*)
                                             (make-default-font))))
                    :color (or color +black+)
-                   :size (or size 18))))
+                   :size (or size 18)
+		   :line-height (or line-height 1.41)
+		   :align (or align :left))))
 
 (defmacro with-font (font &body body)
   (alexandria:once-only (font)
@@ -34,21 +38,44 @@
 (defun set-font (font)
   (setf (env-font *env*) font))
 
+(defun text-scale (resources spacing width height)
+  (let ((rendered-width (apply #'max (mapcar #'image-width resources)))
+	(rendered-height (+ (* (- (length resources) 1) spacing)
+			    (apply #'+ (mapcar #'image-height resources)))))
+    (cond ((and (null width) (null height)) (list 1 1))
+	  ((null width) (list 1 (/ height rendered-height)))
+	  ((null height) (list (/ width rendered-width) 1))
+	  (t (list (/ width rendered-width) (/ height rendered-height))))))
+
+(defun text-align (align width)
+  (cond ((eq align :right) (- width))
+	((eq align :center) (- (round (/ width 2))))
+	(t 0)))
+
 (defun text (text-string x y &optional width height)
   (let* ((font (env-font *env*))
          (typeface (and font (load-resource (typeface-filename (font-face font))
                                             :size (font-size font)))))
-    (when font
-      (destructuring-bind (r g b a) (color-rgba-255 (font-color font))
-	(let ((top 0))
-	  (dolist (line (split-sequence:split-sequence #\newline text-string))
-	    (let ((resource (make-image-from-surface
-			     (sdl2-ttf:render-utf8-blended
-			      (typeface-pointer typeface) line r g b a))))
-	      (with-pen (make-pen :stroke nil)
-		(image resource x (+ y top) width height)
-		(incf top (image-height resource))
-		(gl:delete-textures (list (image-texture resource)))))))))))
+    (when (and font (> (length text-string) 0))
+      (with-pen (make-pen :stroke nil)
+	(destructuring-bind (r g b a) (color-rgba-255 (font-color font))
+	  (let* ((top 0)
+		 (lines (split-sequence:split-sequence #\newline text-string))
+		 (resources (mapcar (lambda (line)
+				      (make-image-from-surface
+				       (sdl2-ttf:render-utf8-blended
+					(typeface-pointer typeface) line r g b a)))
+				    lines))
+		 (spacing (* (font-size font) (font-line-height font)))
+		 (scale (text-scale resources spacing width height)))
+	    (dolist (resource resources)
+	      (image resource
+		     (+ x (text-align (font-align font) (* (first scale) (image-width resource))))
+		     (+ y top)
+		     (* (first scale) (image-width resource))
+		     (* (second scale) (image-height resource)))
+	      (incf top (* (second scale) spacing))
+	      (gl:delete-textures (list (image-texture resource))))))))))
 
 (let ((font))
   (defun make-default-font ()
